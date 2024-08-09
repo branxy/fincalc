319cee9efaa18a3d79f904eb1e25769dfe24341f
branxy <churkin.michael@gmail.com>
2024-08-09 12:14:35 +0300

    fix: CSS: fixed color of `editTableCell` btn in light mode

--- src/components/transactionsTable/cells/EditCellButton.tsx
+++ src/components/transactionsTable/cells/EditCellButton.tsx
@@ -10,10 +10,10 @@ function EditCellButton({ isHovered, setIsEditing }: EditCellButtonProps) {
     <>
       {isHovered && (
         <button
-          className="bg-slate-800 px-2 py-1 rounded-md absolute top-2 right-1"
+          className="absolute right-1 top-2 rounded-md bg-foreground px-2 py-1"
           onClick={() => setIsEditing(true)}
         >
-          <Pencil size={16} />
+          <Pencil size={16} className="text-primary-foreground" />
         </button>
       )}
     </>
